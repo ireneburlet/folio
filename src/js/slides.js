0679999b50f7142500ec89524c2fcd70a501fcf3
ireneburlet <irene.blt@free.fr>
2018-11-06 17:32:21 +0100

    slider arrowright and left done

--- src/js/slides.js
+++ src/js/slides.js
@@ -1,25 +1,64 @@
 (function() {
 	var slider = {};
 
-	slider.slideIndex = 1;
-
+	slider.slideIndex = 0;
+	slider.imgSize = 0;
+	slider.offsetX = 0;
+	slider.nameSlider = '';
 	slider.slides = [];
 
-	slider.openSlider = function(evt){
+	slider.openAndClose = function(evt){
 		var className = '.p'+ evt.currentTarget.dataset.slidenmb;
 		var isActive = $(className).hasClass('active');
 		if(isActive){
+			slider.slideIndex = 0;
+			slider.imgSize = 0;
+			slider.offsetX = 0;
+			$(".img-project").css("transform", "translate3d(0px, " + slider.offsetX + "px, 0px);");
 			$(className).removeClass("active");
 			$('.ctn-buttons-slider').css("opacity", "0");
+			slider.nameSlider = '';
+			slider.slides = null;
 		}
 		else{
+			slider.nameSlider = className;
+			slider.imgSize = $(className).width();
 			$(className).addClass("active");
 			$('.ctn-buttons-slider').css("opacity", "1");
+			slider.slides = $(className).children('.img-project');
+			console.log(slider.slides);
 		}
 	};
 
-	slider.showSlide = function (evt){
-		console.log(evt);
+	slider.changeSlide = function(event){
+		if($(slider.nameSlider).hasClass('active')){
+			if(event.key === "ArrowRight"){
+				slider.nextSlide();
+			}
+			else if(event.key === "ArrowLeft"){
+				slider.previousSlide();
+			}
+			slider.slides.forEach(function(item) {
+				console.log(item);
+				item.style.display = "none";
+				// $(".img-project").css("transform", "translate3d(" + slider.offsetX + "px, 0px, 0px);");
+			});
+			slider.slides[slider.slideIndex].style.display = "block";
+		}
+	};
+
+	slider.nextSlide = function(){
+		slider.slideIndex++;
+		// slider.offsetX += slider.imgSize;
+	};
+
+	slider.previousSlide = function(){
+		slider.slideIndex--;
+		// slider.offsetX -= slider.imgSize;
+	};
+
+	slider.showSlide = function (n){
+		// console.log(evt);
 	};
 
 	slider.goBack = function(evt){
@@ -30,7 +69,6 @@
 	};
 
 	/*--------------- Click slider div event listener -----------------*/
-	$('#projects').on("click", "div", _.throttle(slider.openSlider, 60));
-	$('#projects').on("click", "#back-btn", _.throttle(slider.goBack, 60));
-
+	$('#projects').on("click", "div", _.throttle(slider.openAndClose, 60));
+	document.addEventListener('keydown', slider.changeSlide);
 }());
